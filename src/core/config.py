5f19aa0610bca28ff4c0f501e3fd32a2ddc503cf
avkritsky <fon-fan1@yandex.ru>
2023-10-12 19:58:00 +0300

    add config load

---
+++ src/core/config.py
@@ -0,0 +1,29 @@
+import os
+from datetime import timedelta
+
+from dotenv import load_dotenv
+
+
+load_dotenv(dotenv_path='.env')
+
+# PROJECT SETTINGS
+PROJECT_TITLE = 'TTS'
+PROJECT_DESCRIPTION = 'Telegram technical support by A.V.Kritsky'
+PROJECT_VERSION = '0.0.1a'
+
+# DATABASE SETTINGS
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_SERVER = os.getenv("POSTGRES_SERVER")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_URL = (
+    f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
+    f"@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+)
+
+# JWT
+JWT_SECRET_CODE = os.getenv("JWT_SECRET_CODE")
+JWT_TTL_ACCESS = timedelta(hours=3)
+JWT_TTL_REFRESH = timedelta(days=7)
+JWT_ALGORITHM = 'HS256'
--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from src.core import config
 
 
-async def lifespan():
+async def lifespan(api: FastAPI):
     print('Start APP')
     yield
     print('Exit APP')
@@ -10,5 +11,10 @@ async def lifespan():
 def create_app() -> FastAPI:
     new_app = FastAPI(
         lifespan=lifespan,
+        description=config.PROJECT_DESCRIPTION,
+        version=config.PROJECT_VERSION,
     )
     return new_app
+
+
+app = create_app()
