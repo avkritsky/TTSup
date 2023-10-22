c142232f6d192c4c0b5daa6f0f52f8fab546e28c
avkritsky <fon-fan1@yandex.ru>
2023-10-22 15:06:24 +0300

    working for tickets route

--- src/backend/apis/v1/tickets/root_tickets.py
+++ src/backend/apis/v1/tickets/root_tickets.py
@@ -1,5 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.backend.core.security import get_current_user
+from src.backend.db.models import User
+from src.backend.db.sessions import database
 
 router = APIRouter(
     prefix='/tickets',
@@ -7,3 +11,11 @@ router = APIRouter(
         'tickets',
     ],
 )
+
+
+@router.post('')
+async def create_new_ticket(
+        user: User = Depends(get_current_user),
+        db: AsyncSession = Depends(database.new_session),
+):
+    return {}
--- src/backend/db/models.py
+++ src/backend/db/models.py
@@ -1,8 +1,18 @@
-from sqlalchemy import Integer, String, CheckConstraint
+from datetime import datetime
+
+from sqlalchemy import (
+    Integer,
+    String,
+    CheckConstraint,
+    Boolean,
+    DateTime,
+    ForeignKey,
+)
 from sqlalchemy.orm import (
     DeclarativeBase,
     Mapped,
     mapped_column,
+    relationship,
 )
 
 from src.backend.core import config
@@ -39,3 +49,58 @@ class User(Base):
             'group': self.group,
         }
         return res
+
+
+class Ticket(Base):
+    __tablename__ = 'ticket' if config.IS_PROD else 'ticket_test'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    # who create ticket
+    author_login: Mapped[str] = mapped_column(
+        String(30),
+        unique=True,
+        nullable=False,
+    )
+    # where create ticket
+    create_time: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.now(),
+    )
+    # where was last update
+    last_update: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.now(),
+    )
+    # is ticket closed?
+    is_closed: Mapped[bool] = mapped_column(Boolean, default=False)
+    # ticket states
+    state: Mapped[str] = mapped_column(
+        String(13),
+        CheckConstraint(
+            '"state" in ' + "('new', 'await user', 'await support', 'closed')"
+        ),
+        nullable=False,
+    )
+
+    dialogs: Mapped[list["TicketDialog"]] = relationship(
+        back_populates="ticket",
+        cascade="all, delete-orphan",
+    )
+
+
+class TicketDialog(Base):
+    __tablename__ = 'ticket_dialog' if config.IS_PROD else 'ticket_dialog_test'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    ticket_id: Mapped[int] = mapped_column(
+        ForeignKey(f"{Ticket.__tablename__}.id")
+    )
+
+    text: Mapped[str] = mapped_column(String(255), nullable=False)
+    author: Mapped[str] = mapped_column(String(30), nullable=False)
+    create_time: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.now(),
+    )
+
+    ticket: Mapped[Ticket] = relationship(back_populates="dialogs")
